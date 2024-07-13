39d761f6891c035270163d21d0745bb02fa92f98
pranayrangne047 <pranay.r@allabroadeducation.com>
2024-07-13 15:08:24 +0530

    login page to index

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/verified" element={<Verified />} />
         <Route path="/trail" element={<TrailPage />} />
       </Routes>
